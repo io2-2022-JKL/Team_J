a8e33abcab61af97130d8a20f0fd47b165d8a4b6
OhMyGodcat <mateusz.garczynski.stud@pw.edu.pl>
2022-03-26 19:02:18 +0100

    Create classes for code-first DB

---
+++ VaccinationSystem/VaccinationSystem/Models/TimeSlot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VaccinationSystem.Models
+{
+    public class TimeSlot
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime From { get; set; }
+        [Required]
+        public DateTime To { get; set; }
+        [ForeignKey("Doctor")]
+        public int DoctorId { get; set; }
+        public Doctor Doctor { get; set; }
+        [Required]
+        public bool IsFree { get; set; }
+        [Required]
+        public bool Active { get; set; }
+    }
+}
