df93ca93b1b6d9a4d5f00eee6d3c94e006b6df8b
Jakub Gazewski <01151590@pw.edu.pl>
2022-04-10 22:47:05 +0200

    Added endpoint functionality
    Fixed endpoint routing, added:
    -show all patients, add patient, delete patient endpoints
    -show all doctors, add doctor, delete doctor endpoints

--- Backend/VaccinationSystem/VaccinationSystem/Controllers/AdminController.cs
+++ Backend/VaccinationSystem/VaccinationSystem/Controllers/AdminController.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using VaccinationSystem.DTO;
 using VaccinationSystem.DTO.PatientDTOs;
 using VaccinationSystem.DTO.AdminDTOs;
+using VaccinationSystem.Config;
+using VaccinationSystem.Models;
 
 namespace VaccinationSystem.Controllers
 {
@@ -14,10 +16,39 @@ namespace VaccinationSystem.Controllers
     [Route("admin")]
     public class AdminController : ControllerBase
     {
+        private readonly VaccinationSystemDbContext _context;
+
+        public AdminController(VaccinationSystemDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet("patients")]
         public ActionResult<IEnumerable<PatientDTO>> GetPatients()
         {
-            return NotFound();
+            var result = GetAllPatients();
+            if (result != null)
+                return Ok(result);
+            return StatusCode(404);
+        }
+
+        private IEnumerable<PatientDTO> GetAllPatients()
+        {
+            List<PatientDTO> result = new List<PatientDTO>();
+            foreach(Patient patient in _context.Patients.ToList())
+            {
+                PatientDTO patientDTO = new PatientDTO();
+                patientDTO.id = patient.Id.ToString();
+                patientDTO.PESEL = patient.PESEL;
+                patientDTO.firstName = patient.FirstName;
+                patientDTO.lastName = patient.LastName;
+                patientDTO.mail = patient.Mail;
+                patientDTO.dateOfBirth = patient.DateOfBirth.ToString("dd-MM-yyyy");
+                patientDTO.phoneNumber = patient.PhoneNumber;
+                patientDTO.active = patient.Active;
+                result.Add(patientDTO);
+            }
+            return result;
         }
 
         [HttpPost("patients/editPatient")]
@@ -29,13 +60,90 @@ namespace VaccinationSystem.Controllers
         [HttpDelete("patients/deletePatient/{patientId}")]
         public IActionResult DeletePatient(string patientId)
         {
-            return NotFound();
+            var result = FindAndDeletePatient(patientId);
+            return result;
+        }
+
+        private IActionResult FindAndDeletePatient(string patientId)
+        {
+            Guid id;
+            try
+            {
+                id = Guid.Parse(patientId);
+            }
+            catch(FormatException)
+            {
+                return StatusCode(404);
+            }
+            catch(ArgumentNullException)
+            {
+                return StatusCode(404);
+            }
+            Patient patient;
+            if((patient = _context.Patients.Where(patient => patient.Active == true && patient.Id == id).FirstOrDefault()) != null)
+            {
+                Doctor doctor;
+                if((doctor = _context.Doctors.Where(doc => doc.Active == true && doc.PatientAccount.Id == id).FirstOrDefault()) != null)
+                {
+                    doctor.Active = false;
+                    foreach(Appointment appointment in doctor.Vaccinations)
+                    {
+                        if(appointment.State == AppointmentState.Planned)
+                        {
+                            appointment.State = AppointmentState.Cancelled;
+                            appointment.TimeSlot.Active = false;
+                            //powiadomić pacjentów
+                        }
+                    }
+                    foreach(TimeSlot timeSlot in _context.TimeSlots.Where(slot => slot.DoctorId == doctor.Id && slot.Active == false).ToList())
+                    {
+                        timeSlot.Active = false;
+                    }
+                }
+                patient.Active = false;
+                if(_context.SaveChanges() < 1)
+                {
+                    return StatusCode(404);
+                }
+                else
+                {
+                    return StatusCode(200);
+                }
+            }
+            return StatusCode(404);
+            
         }
 
         [HttpGet("doctors")]
         public ActionResult<IEnumerable<GetDoctorsResponseDTO>> GetDoctors()
         {
-            return NotFound();
+            var result = GetAllDoctors();
+            if (result != null)
+                return Ok(result);
+            return StatusCode(404);
+        }
+
+        private IEnumerable<GetDoctorsResponseDTO> GetAllDoctors()
+        {
+            List<GetDoctorsResponseDTO> result = new List<GetDoctorsResponseDTO>();
+            foreach(Doctor doc in _context.Doctors.ToList())
+            {
+                GetDoctorsResponseDTO getDoctorsResponseDTO = new GetDoctorsResponseDTO();
+                getDoctorsResponseDTO.id = doc.Id.ToString();
+                getDoctorsResponseDTO.PESEL = doc.PatientAccount.PESEL;
+                getDoctorsResponseDTO.firstName = doc.PatientAccount.FirstName;
+                getDoctorsResponseDTO.lastName = doc.PatientAccount.LastName;
+                getDoctorsResponseDTO.mail = doc.PatientAccount.Mail;
+                getDoctorsResponseDTO.dateOfBirth = doc.PatientAccount.DateOfBirth.ToString("dd-MM-yyyy");
+                getDoctorsResponseDTO.phoneNumber = doc.PatientAccount.PhoneNumber;
+                getDoctorsResponseDTO.active = doc.Active;
+                getDoctorsResponseDTO.vaccinationCenterId = doc.VaccinationCenterId.ToString();
+                getDoctorsResponseDTO.name = doc.VaccinationCenter.Name;
+                getDoctorsResponseDTO.city = doc.VaccinationCenter.City;
+                getDoctorsResponseDTO.street = doc.VaccinationCenter.Address;
+                result.Add(getDoctorsResponseDTO);
+            }
+            return result;
         }
 
         [HttpPost("doctors/editDoctor")]
@@ -47,13 +155,115 @@ namespace VaccinationSystem.Controllers
         [HttpPost("doctors/addDoctor")]
         public IActionResult AddDoctor(AddDoctorRequestDTO addDoctorRequestDTO)
         {
-            return NotFound();
+            var result = AddNewDoctor(addDoctorRequestDTO);
+            return result;
+        }
+
+        private IActionResult AddNewDoctor(AddDoctorRequestDTO addDoctorRequestDTO)
+        {
+            Guid id;
+            try
+            {
+                id = Guid.Parse(addDoctorRequestDTO.doctorId);
+            }
+            catch (FormatException)
+            {
+                return StatusCode(404);
+            }
+            catch (ArgumentNullException)
+            {
+                return StatusCode(404);
+            }
+            Doctor doctor = new Doctor();
+            doctor.PatientAccount = _context.Patients.Where(patient => patient.Active == true && patient.Id == id).FirstOrDefault();
+            if(doctor.PatientAccount == null)
+            {
+                return StatusCode(404);
+            }
+            Guid vcId;
+            try
+            {
+                vcId = Guid.Parse(addDoctorRequestDTO.vaccinationCenterId);
+            }
+            catch (FormatException)
+            {
+                return StatusCode(404);
+            }
+            catch (ArgumentNullException)
+            {
+                return StatusCode(404);
+            }
+            doctor.VaccinationCenterId = vcId;
+            doctor.VaccinationCenter = _context.VaccinationCenters.Where(vc => vc.Active == true && vc.Id == vcId).FirstOrDefault();
+            if(doctor.VaccinationCenter == null)
+            {
+                return StatusCode(404);
+            }
+            doctor.Vaccinations = new List<Appointment>();
+            var entry = _context.Doctors.Add(doctor);
+            if (entry.State != Microsoft.EntityFrameworkCore.EntityState.Added)
+            {
+                return StatusCode(404);
+            }
+            if (_context.SaveChanges() < 1)
+            {
+                return StatusCode(404);
+            }
+            return StatusCode(200);
+
         }
 
         [HttpDelete("doctors/deleteDoctor/{doctorId}")]
         public IActionResult DeleteDoctor(string doctorId)
         {
-            return NotFound();
+            var result = FindAndDeleteDoctor(doctorId);
+            return result;
+        }
+
+        private IActionResult FindAndDeleteDoctor(string doctorId)
+        {
+            Guid id;
+            try
+            {
+                id = Guid.Parse(doctorId);
+            }
+            catch (FormatException)
+            {
+                return StatusCode(404);
+            }
+            catch (ArgumentNullException)
+            {
+                return StatusCode(404);
+            }
+
+            Doctor doctor;
+            if((doctor = _context.Doctors.Where(doc => doc.Active == true && doc.Id == id).SingleOrDefault()) != null)
+            {
+                doctor.Active = false;
+                foreach (Appointment appointment in doctor.Vaccinations)
+                {
+                    if (appointment.State == AppointmentState.Planned)
+                    {
+                        appointment.State = AppointmentState.Cancelled;
+                        appointment.TimeSlot.Active = false;
+                        //powiadomić pacjentów
+                    }
+                }
+                foreach (TimeSlot timeSlot in _context.TimeSlots.Where(slot => slot.DoctorId == doctor.Id && slot.Active == false).ToList())
+                {
+                    timeSlot.Active = false;
+                }
+                if (_context.SaveChanges() < 1)
+                {
+                    return StatusCode(404);
+                }
+                else
+                {
+                    return StatusCode(200);
+                }
+            }
+            return StatusCode(404);
+
         }
 
         [HttpGet("vaccinationCenters")]
--- Backend/VaccinationSystem/VaccinationSystem/Controllers/DefaultController.cs
+++ Backend/VaccinationSystem/VaccinationSystem/Controllers/DefaultController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using VaccinationSystem.Config;
@@ -25,13 +26,120 @@ namespace VaccinationSystem.Controllers
         [HttpPost("register")]
         public IActionResult RegisterUser(RegisterRequestDTO registerRequestDTO)
         {
-            return NotFound();
+            var result = AddNewUser(registerRequestDTO);
+            return result;
+        }
+
+        private IActionResult AddNewUser(RegisterRequestDTO registerRequestDTO)
+        {
+            Patient patient = new Patient();
+            patient.PESEL = registerRequestDTO.PESEL;
+            if(patient.PESEL.Length != 11 || !int.TryParse(patient.PESEL, out _))
+            {
+                return StatusCode(400);
+            }
+            if(_context.Patients.Where(u=>u.PESEL == patient.PESEL).Any())
+            {
+                return StatusCode(400);
+            }
+            patient.FirstName = registerRequestDTO.names;
+            if(ContainsSymbol(patient.FirstName))
+            {
+                return StatusCode(400);
+            }
+            patient.LastName = registerRequestDTO.surname;
+            if(ContainsSymbol(patient.LastName))
+            {
+                return StatusCode(400);
+            }
+            try
+            {
+                patient.DateOfBirth = DateTime.ParseExact(registerRequestDTO.dateOfBirth, "dd-MM-yyyy", null);
+            }
+            catch(FormatException)
+            {
+                return StatusCode(400);
+            }
+            patient.Mail = registerRequestDTO.mail;
+            if(!new EmailAddressAttribute().IsValid(patient.Mail))
+            {
+                return StatusCode(400);
+            }
+            patient.Password = registerRequestDTO.password;
+            patient.PhoneNumber = new string(registerRequestDTO.phoneNumber.ToCharArray().Where(c => !Char.IsWhiteSpace(c)).ToArray());
+            if(!IsPhoneNumber(patient.PhoneNumber))
+            {
+                return StatusCode(400);
+            }
+            patient.Vaccinations = new List<Appointment>();
+            patient.Certificates = new List<Certificate>();
+            patient.Active = true;
+            var entry = _context.Patients.Add(patient);
+            if(entry.State != Microsoft.EntityFrameworkCore.EntityState.Added)
+            {
+                return StatusCode(400);
+            }
+            if(_context.SaveChanges() != 1)
+            {
+                return StatusCode(400);
+            }
+            return StatusCode(200);
+        }
+
+        private bool ContainsSymbol(string s)
+        {
+            return s.IndexOfAny("!@#$%^&*()_+=[{]};:\"\\|<>,./".ToCharArray()) != -1;
+        }
+        private bool IsPhoneNumber(string s)
+        {
+            
+            if (s.Length > 15)
+                return false;
+            if (s[0] == '+')
+            {
+                if (!int.TryParse(s.Substring(1), out _))
+                    return false;
+            }
+            else
+            {
+                if (!int.TryParse(s, out _))
+                    return false;
+            }
+            return true;
         }
 
         [HttpPost("signin")]
         public ActionResult<SigninResponseDTO> SignInUser(SigninRequestDTO signinRequestDTO)
         {
-            return NotFound();
+            var result = FindUser(signinRequestDTO);
+            if (result != null)
+                return Ok(result);
+            return StatusCode(400);
+        }
+
+        private SigninResponseDTO FindUser(SigninRequestDTO signinRequestDTO)
+        {
+            SigninResponseDTO result = new SigninResponseDTO();
+            User account;
+            if ((account = _context.Doctors.Where(doc => doc.PatientAccount.Active == true && doc.PatientAccount.Mail == signinRequestDTO.mail && doc.PatientAccount.Password == signinRequestDTO.password).SingleOrDefault().PatientAccount) != null)
+            {
+                result.userID = account.Id.ToString();
+                result.userType = "doctor";
+                return result;
+            }
+            if((account = _context.Patients.Where(patient => patient.Active == true && patient.Mail == signinRequestDTO.mail && patient.Password == signinRequestDTO.password).SingleOrDefault()) != null)
+            {
+                result.userID = account.Id.ToString();
+                result.userType = "patient";
+                return result;
+            }
+            if((account = _context.Admins.Where(admin => admin.Mail == signinRequestDTO.mail && admin.Password == signinRequestDTO.password).SingleOrDefault()) != null)
+            {
+                result.userID = account.Id.ToString();
+                result.userType = "admin";
+                return result;
+            }
+            return null;
         }
 
         [HttpPost("user/logout/{userId}")]
