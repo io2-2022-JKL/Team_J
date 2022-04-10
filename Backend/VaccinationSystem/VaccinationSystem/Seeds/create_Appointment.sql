9510221a12a24394e64a8716e14fb43212af3d1c
Zyrtek <mateusz.garczynski.stud@pw.edu.pl>
2022-04-10 22:39:04 +0200

    Added additional .sql seeds

---
+++ Backend/VaccinationSystem/VaccinationSystem/Seeds/create_Appointment.sql
@@ -0,0 +1,10 @@
+  INSERT INTO [dbo].[Appointment] ([Id]
+      ,[WhichDose]
+      ,[TimeSlotId]
+      ,[PatientId]
+      ,[VaccineId]
+      ,[State]
+      ,[VaccineBatchNumber]
+      ,[DoctorId])
+	VALUES ('9e4aaa7e-b891-44fd-b62a-9dbd1d36f3bf', 1, 'b1351457-ed07-4da8-88f2-423a44b8b340', 'f969ffd0-6dbc-4900-8eb8-b4fe25906a74',
+    'ed200efa-5007-4f63-b93c-4701712c16b7', 1, null, '6be3ea9d-8af7-4552-896d-e162248931d5')
