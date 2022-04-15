a395ad798d0bff21c4f067488bd557d40b71427d
adamWodz <01151602@pw.edu.pl>
2022-04-15 19:23:15 +0200

    added FilteringHelpers.js and AdminApi.js

---
+++ Frontend/vaccination-system/src/components/FilteringHelepers.js
@@ -0,0 +1,38 @@
+export class FilteringHelepers {
+    static filterFirstName(array, filter) {
+        return array.filter((item) => item.firstName.includes(filter));
+    };
+    static filterLastName(array, filter) {
+        return array.filter((item) => item.lastName.includes(filter));
+    };
+    static filterEmail(array, filter) {
+        return array.filter((item) => item.email.includes(filter));
+    };
+    static filterPhoneNumber(array, filter) {
+        return array.filter((item) => item.phoneNumber.includes(filter));
+    };
+    static filterPESEL(array, filter) {
+        return array.filter((item) => item.PESEL.includes(filter));
+    };
+    static filterId(array, filter) {
+        return array.filter((item) => item.id.includes(filter));
+    };
+    static filterDate(array, filter) {
+        return array.filter((item) => {
+            if (filter.length !== 10)
+                return true;
+            else
+                return item.dateOfBirth === filter;
+        });
+    }
+    static filterActive(array, filter) {
+        return array.filter((item) => {
+            if (filter === "aktywny")
+                return item.active === true;
+            else if (filter === "nieaktywny")
+                return item.active === false;
+            else
+                return true;
+        });
+    }
+}
