5a6ba4aedc05d1de9e410b452ca1f9020b8a5efa
adamWodz <01151602@pw.edu.pl>
2022-04-23 17:52:31 +0200

    updated login page

--- Frontend/vaccination-system/src/components/Patient/PatientApi.js
+++ Frontend/vaccination-system/src/components/Patient/PatientApi.js
@@ -1,28 +1,23 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Container, CssBaseline, TextField } from '@mui/material';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { useNavigate } from "react-router-dom";
-import Grid from "@material-ui/core/Grid";
-import { DataGrid, GridActionsCellItem } from '@mui/x-data-grid';
-import { randomDate, randomEmail, randomId, randomPhoneNumber, randomTraderName, randomBoolean, randomInt } from '@mui/x-data-grid-generator';
-import dateFormat from 'dateformat';
-import DeleteIcon from '@mui/icons-material/Delete';
-import clsx from 'clsx';
-import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
-import Avatar from '@mui/material/Avatar';
-import { confirm } from "react-confirm-box";
-import { PlayCircleFilledWhiteRounded } from '@mui/icons-material';
 import axios from 'axios';
-import LinearProgress from '@mui/material/LinearProgress';
-import DataDisplayArray from '../DataDisplayArray';
 
-export async function getFreeTimeSlots() {
+export async function getFreeTimeSlots(city, virus, dateFrom, dateTo) {
+
+    let response;
     try {
-        const { data: response } = await axios.get('https://localhost:5001/admin/patients');
-        return response;
+        response = await axios({
+            method: 'post',
+            url: 'https://systemszczepien.azurewebsites.net/patient/timeSlots/Filter',
+
+            data: {
+                city: "Warszawa",
+                dateFrom: "01-01-2022 10:00",
+                dateTo: "01-10-2022 10:00",
+                virus: "Koronawirus"
+            }
+        });
+        console.log({
+            response,
+        })
     } catch (error) {
         console.error(error.message);
     }
