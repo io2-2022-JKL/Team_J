d849303bb5b7545b1fd933c71bb950ede011f805
Jakub Gazewski <01151590@pw.edu.pl>
2022-06-12 18:56:42 +0200

    changed uri from local to deployed

--- Backend/VaccinationSystem/VaccinationSystem/Controllers/AdminController.cs
+++ Backend/VaccinationSystem/VaccinationSystem/Controllers/AdminController.cs
@@ -30,7 +30,7 @@ namespace VaccinationSystem.Controllers
         private readonly string _dateTimeFormat = "dd-MM-yyyy HH\\:mm";
         private readonly string _timeSpanFormat = "hh\\:mm";
         private readonly IHttpClientFactory _httpClientFactory;
-        private readonly string _baseUri = "https://localhost:6001";
+        private readonly string _baseUri = "https://systemszczepienkonta.azurewebsites.net/";
 
         public AdminController(VaccinationSystemDbContext context, IHttpClientFactory httpClientFactory)
         {
